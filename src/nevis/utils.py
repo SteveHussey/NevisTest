ea7ded00c3dca5e5bfc5af4e3b03641af5dcb69c
SteveH <94375670+stevehussey@users.noreply.github.com>
2026-01-23 00:19:52 +0000

    ID Generator added
    Generates alphanumeric IDs, will be used for Client and Document IDs.

--- src/nevis/utils.py
+++ src/nevis/utils.py
@@ -1,4 +1,22 @@
+import string
+from collections.abc import Generator
+from random import Random
+
 from nevis.models import ClientId, DocumentId
+
+
+def id_generator(length: int, seed=None) -> Generator[str, None, None]:
+    """Generate sequential alphanumeric IDs of a fixed length.
+
+    Uses characters 0-9, A-Z, a-z as the base-62 alphabet.
+    """
+    if length <= 0:
+        raise ValueError("Length must be a positive integer")
+
+    population = string.digits + string.ascii_uppercase + string.ascii_lowercase
+    rand = Random(seed)
+    while True:
+        yield "".join(rand.choices(population, k=length))
 
 
 class NevisException(Exception):
@@ -19,3 +37,9 @@ class InvalidDocumentError(NevisException):
     def __init__(self, client_id: ClientId, document_id: DocumentId):
         self.client_id = client_id
         self.document_id = document_id
+
+
+if __name__ == '__main__':
+    gen = id_generator(8)
+    for _ in range(10):
+        print(next(gen))
